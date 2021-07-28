bc8414194af3c54865ef4f564645f00b7e0769a3
Gautam Savaliya <gautam.savaliya@newpage.io>
2021-07-28 17:36:47 +0530

    default single public and private subnet

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "public_subnets" {
   type        = list(string)
   description = "Network ranges for public subnets created in the VPC. Able to access the internet through Default Internet gateway."
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+  default     = ["10.0.101.0/24"]
 }
 
 variable "private_subnets" {
   type        = list(string)
   description = "Network ranges for private subnets created in the VPC. Able to access the internet through Cloud Router and NAT Gateway."
-  default     = ["10.0.103.0/24", "10.0.104.0/24"]
+  default     = ["10.0.102.0/24"]
 }
