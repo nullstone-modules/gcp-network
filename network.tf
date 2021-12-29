582863cd4ac60dc5b58df9867adf08b042405267
Brad Sickles <brad.sickles@gmail.com>
2021-12-29 12:36:33 -0500

    Fix network and organize subnets into locals

--- network.tf
+++ network.tf
@@ -1,37 +1,31 @@
+locals {
+  public_subnets = [for index, subnet_cidr in var.public_subnets : {
+    subnet_name   = "${local.resource_name}-public-${index + 1}"
+    subnet_ip     = subnet_cidr
+    subnet_region = data.google_compute_zones.available.region
+  }]
+
+  private_subnets = [for index, subnet_cidr in var.private_subnets : {
+    subnet_name           = "${local.resource_name}-private-${index + 1}"
+    subnet_ip             = subnet_cidr
+    subnet_region         = data.google_compute_zones.available.region
+    subnet_private_access = "true"
+  }]
+}
+
 module "gcp-network" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 3.0"
+  version = "~> 4.0"
 
   project_id   = data.google_compute_zones.available.project
   network_name = local.resource_name
   routing_mode = "GLOBAL"
-
-  subnets = concat(
-    # Public subnet
-    flatten([
-      for index, subnet_cidr in var.public_subnets : {
-        subnet_name   = "${local.resource_name}-public-${index + 1}"
-        subnet_ip     = subnet_cidr
-        subnet_region = data.google_compute_zones.available.region
-      }
-    ]),
-
-    # Private subnet
-    flatten([
-      for index, subnet_cidr in var.private_subnets : {
-        subnet_name           = "${local.resource_name}-private-${index + 1}"
-        subnet_ip             = subnet_cidr
-        subnet_region         = data.google_compute_zones.available.region
-        subnet_private_access = "true"
-      }
-    ])
-  )
+  subnets      = concat(local.public_subnets, local.private_subnets)
 }
 
 resource "google_compute_router_nat" "nat" {
-  depends_on = [
-    module.gcp-network
-  ]
+  depends_on = [module.gcp-network]
+
   name                               = "${module.gcp-network.network_name}-nat"
   router                             = google_compute_router.router.name
   region                             = data.google_compute_zones.available.region
@@ -51,6 +45,7 @@ resource "google_compute_router" "router" {
   name    = "${module.gcp-network.network_name}-router"
   network = module.gcp-network.network_name
   region  = data.google_compute_zones.available.region
+
   bgp {
     asn            = 64514
     advertise_mode = "CUSTOM"
