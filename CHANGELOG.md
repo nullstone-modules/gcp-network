1463e905dbf0c97f88bd786276721b16cb5ab438
Brad Sickles <brad.sickles@gmail.com>
2023-03-22 15:31:02 -0400

    Add .terraform.lock.hcl, publish module using CLI, add module manifest

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# 0.2.0 (Mar 22, 2023)
+* Add `.terraform.lock.hcl`.
--- nullstone.tf
+++ nullstone.tf
@@ -8,14 +8,17 @@ terraform {
 
 data "ns_workspace" "this" {}
 
+// Generate a random suffix to ensure uniqueness of resources
 resource "random_string" "resource_suffix" {
   length  = 5
   lower   = true
   upper   = false
-  number  = false
+  numeric = false
   special = false
 }
 
 locals {
+  tags          = data.ns_workspace.this.tags
+  block_name    = data.ns_workspace.this.block_name
   resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
 }
