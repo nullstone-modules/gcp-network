a81cba8a2be0148ba764769632be31fb49eb04a2
Scott Sickles <scottsickles@gmail.com>
2023-05-10 00:29:06 -0400

    add google_project_service to enable compute api

--- gcp.tf
+++ gcp.tf
@@ -1,1 +1,7 @@
 data "google_compute_zones" "available" {}
+
+resource "google_project_service" "compute" {
+  service                    = "compute.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
