aaa65a9234d7542158680dab59c39b8743681944
Brad Sickles <brad.sickles@gmail.com>
2023-06-12 14:49:02 -0400

    Fix destroy ordering

--- serverless-connector.tf
+++ serverless-connector.tf
@@ -1,5 +1,6 @@
 resource "google_vpc_access_connector" "serverless" {
-  count = 1
+  count      = 1
+  depends_on = [google_compute_network.this]
 
   name         = "${local.resource_name}-${count.index}"
   machine_type = "e2-micro"
